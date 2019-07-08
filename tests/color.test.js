747ba61f5e190f9fac72253c9fdb409e4cc8544c
alechp <me@alechp.com>
2019-07-08 16:50:06 -0700

    confirmed getChalkColor

--- tests/color.test.js
+++ tests/color.test.js
@@ -3,7 +3,13 @@ import test from "ava";
 import execa from "execa";
 import { getChalkColor, printLine, printMirror } from "../src/index";
 
-test("print a blue line", async t => {
+test("print a blue line", t => {
   let foo = printLine("blue");
   t.snapshot(foo);
+});
+
+test("print inner function of getChalkColor", t => {
+  let getChalkColorFunction = getChalkColor("blue");
+  printMirror({ getChalkColorFunction }, "blue", "grey");
+  t.snapshot(getChalkColorFunction);
 });
